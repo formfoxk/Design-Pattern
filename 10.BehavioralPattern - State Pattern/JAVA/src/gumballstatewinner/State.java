d5588c6cbb7cd6c2e26d049f4a703e4cef83185f
yujoo <formfoxk@gmail.com>
2015-11-17 23:59:22 +0900

    10. State Pattern

---
+++ 10.BehavioralPattern - State Pattern/JAVA/src/gumballstatewinner/State.java
@@ -0,0 +1,12 @@
+package gumballstatewinner;
+
+
+public interface State {
+ 
+	public void insertQuarter();
+	public void ejectQuarter();
+	public void turnCrank();
+	public void dispense();
+	
+	public void refill();
+}

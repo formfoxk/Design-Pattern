bbc484b0e898cf8c1853393824f50d79d4fcf5f2
yujoo <formfoxk@gmail.com>
2015-12-02 22:32:14 +0900

    13. Command Pattern

---
+++ 12.StructuralPattern - Composite Pattern/JAVA/src/dirsearch/DirSearchTestMain.java
@@ -0,0 +1,9 @@
+package dirsearch;
+
+public class DirSearchTestMain {
+	
+	public static void main(String[] args){
+		DirSearcher dirSearcher = new DirSearcher("D:/OneDrive/학교_수강/3.2 디자인 패턴/실습 보고서/디자인패턴 실습보고서(1) UML");
+		dirSearcher.printAll();
+	}
+}
